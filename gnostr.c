1acd7e90cd02004c0492829e3fc02d758d26dd51
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-10-27 19:33:22 -0400

    gnostr.c:
    // printf("TODO:handle gnostr-sub-sub");

--- gnostr.c
+++ gnostr.c
@@ -767,7 +767,11 @@ static void try_subcommand(int argc, const char *argv[])
 	if (strlen(sub) >= 1 && sub[0] != '-') {
 		snprintf(buf, sizeof(buf)-1, "gnostr-%s", sub);
 		execvp(buf, (char * const *)argv+1);
-	}
+  } else {
+
+    // printf("TODO:handle gnostr-sub-sub");
+
+  }
 }
 
 

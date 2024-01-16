bef1a8f342d9ce9ea965e056d560b2292634657b
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-01-15 20:30:05 -0500

    build.rs:build_tui_command

--- build.rs
+++ build.rs
@@ -1,62 +1,38 @@
-use std::path::PathBuf;
-use std::{env};
+// build.rs
+
+use std::process::Command;
 
 fn main() {
-    let include_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    let mut shared_object_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    shared_object_dir.push("target");
-    shared_object_dir.push(env::var("PROFILE").unwrap());
-    let shared_object_dir = shared_object_dir.as_path().to_string_lossy();
+	let build_tui_command = if cfg!(target_os = "windows") {
+		Command::new("cmd")
+			.args(["/C", "cargo", "install", "--path", "tui"])
+			.output()
+			.expect("failed to execute process")
+	} else if cfg!(target_os = "macos") {
+		Command::new("sh")
+			.arg("-c")
+			.args(["cargo", "install", "--path", "tui"])
+			.output()
+			.expect("failed to execute process")
+	} else if cfg!(target_os = "linux") {
+		Command::new("sh")
+			.arg("-c")
+			.args(["cargo", "install", "--path", "tui"])
+			.output()
+			.expect("failed to execute process")
+	} else {
+		Command::new("sh")
+			.arg("-c")
+			.args(["cargo", "install", "--path", "tui"])
+			.output()
+			.expect("failed to execute process")
+	};
 
-    // The following options mean:
-    //
-    // * `-I`, add `include_dir` to include search path,
-    // * `-L`, add `shared_object_dir` to library search path,
-    // * `-D_DEBUG`, enable debug mode to enable `assert.h`.
-    println!(
-        "cargo:rustc-env=INLINE_C_RS_CFLAGS=-I{I} -L{L} -D_DEBUG",
-        I = include_dir,
-        L = shared_object_dir.clone(),
-    );
-
-    // Here, we pass the fullpath to the shared object with
-    // `LDFLAGS`.
-    println!(
-        "cargo:rustc-env=INLINE_C_RS_LDFLAGS={shared_object_dir}/{lib}",
-        shared_object_dir = shared_object_dir,
-        lib = if cfg!(target_os = "windows") {
-            "foo.dll".to_string()
-        } else if cfg!(target_os = "macos") {
-            "libfoo.dylib".to_string()
-        } else {
-            "libfoo.so".to_string()
-        }
-    );
+	let _build_tui = String::from_utf8(build_tui_command.stdout)
+		.map_err(|non_utf8| {
+			String::from_utf8_lossy(non_utf8.as_bytes()).into_owned()
+		})
+		.unwrap();
 }
 
-////https://docs.rs/inline-c/latest/inline_c/#meta-environment-variables
-//fn main() {
-//    set_var("INLINE_C_RS_FOO", "bar baz qux");
-//
-//    (assert_c! {
-//        #include <stdio.h>
-//        #include <stdlib.h>
-//
-//        int main() {
-//            const char* foo = getenv("FOO");
-//
-//            if (NULL == foo) {
-//                return 1;
-//            }
-//
-//            printf("FOO is set to `%s`", foo);
-//
-//            return 0;
-//        }
-//    })
-//    .success()
-//    .stdout("FOO is set to `bar baz qux`");
-//
-//    remove_var("INLINE_C_RS_FOO");
-//}
